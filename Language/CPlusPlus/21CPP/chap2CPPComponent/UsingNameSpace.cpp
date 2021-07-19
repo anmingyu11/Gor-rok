8eeaaed8f000354ae0f488c9d0bb8c23782f86de
anmingyu <anmingyu@baidu.com>
2021-07-19 13:36:00 +0800

    [21Cpp] Chap2 Cpp Component.
    Signed-off-by: anmingyu <anmingyu@baidu.com>

---
+++ Language/CPlusPlus/21CPP/chap2CPPComponent/UsingNameSpace.cpp
@@ -0,0 +1,7 @@
+# include <iostream>
+
+int main(){
+	using namespace std;
+    cout << "HelloWorld" << endl;
+    return 0;
+}
---
+++ Language/CPlusPlus/21CPP/chap2CPPComponent/stdinout.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int InputNumber;
+    cout << "enter : " << endl;
+    cin >> InputNumber;
+
+    string name;
+    cout << "enter name : " << endl;
+    cin >> name;
+
+    cout << "name : " << name << "number:" << InputNumber << endl;
+    return 0;
+}
