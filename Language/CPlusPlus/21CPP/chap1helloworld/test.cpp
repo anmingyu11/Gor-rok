3f0a622588e1db142708af672b133c9776fe5789
anmingyu <anmingyu@baidu.com>
2021-08-16 22:19:51 +0800

    [21cpp] HelloWorld
    Signed-off-by: anmingyu <anmingyu@baidu.com>

---
+++ Language/CPlusPlus/21CPP/chap1helloworld/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    uint32_t diff = 189239;
+
+    uint32_t v = (uint32_t)((double)diff / 3600.0 / 24.0);
+
+    cout << v << endl;
+
+
+    return 0;
+}
